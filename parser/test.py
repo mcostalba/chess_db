3e8229fd6f0bddebfb3cb5f7cac4f61198924459
Marco Costalba <Mcostalba@gmail.com>
2016-12-04 09:27:33 +0000

    Fix some pep8 warnings

--- parser/test.py
+++ parser/test.py
@@ -7,33 +7,33 @@ import os
 import sys
 from subprocess import STDOUT, check_output as qx
 
-DB = {'GM_games'               : {'games':  20, 'moves':  1519, 'fixed':  0},
-      'ambiguous'              : {'games':   3, 'moves':   194, 'fixed':  2},
-      'bali02'                 : {'games':  16, 'moves':  1485, 'fixed':  0},
-      'chessdoctor'            : {'games':  55, 'moves':  4648, 'fixed':249},
-      'd00_chess_informant'    : {'games': 103, 'moves':  7019, 'fixed':  0},
-      'electronic_campfire'    : {'games':   6, 'moves':   286, 'fixed':  2},
-      'europe_echecs'          : {'games': 307, 'moves': 21024, 'fixed': 15},
-      'exeter_lessons_from_tal': {'games':   8, 'moves':   628, 'fixed':  0},
-      'famous_games'           : {'games': 500, 'moves': 28796, 'fixed': 60},
-      'great_masters'          : {'games':  13, 'moves':   872, 'fixed': 34},
-      'hartwig'                : {'games':  29, 'moves':  2203, 'fixed':  2},
-      'hayes'                  : {'games':   9, 'moves':   637, 'fixed':  0},
-      'human_computer'         : {'games':  29, 'moves':  2290, 'fixed':  0},
-      'immortal_games'         : {'games':  36, 'moves':  2026, 'fixed':  8},
-      'kramnik'                : {'games':  40, 'moves':  3293, 'fixed':  1},
-      'middleg'                : {'games': 481, 'moves': 31560, 'fixed': 19},
-      'moscow64'               : {'games':  42, 'moves':  3760, 'fixed':  0},
-      'newyork1924'            : {'games': 110, 'moves': 10517, 'fixed': 27},
-      'perle'                  : {'games': 194, 'moves': 14922, 'fixed': 11},
-      'polgar'                 : {'games':  40, 'moves':  3348, 'fixed':  0},
-      'pon_korch'              : {'games':   8, 'moves':  1003, 'fixed':  0},
-      'romero'                 : {'games':   4, 'moves':   363, 'fixed':  0},
-      'russian_chess'          : {'games':  10, 'moves':   725, 'fixed':  8},
-      'scarborough_2001'       : {'games': 385, 'moves': 30137, 'fixed': 15},
-      'scca'                   : {'games':  75, 'moves':  5114, 'fixed':  6},
-      'schiller'               : {'games':   8, 'moves':   681, 'fixed':  0},
-      'semicomm'               : {'games':  46, 'moves':  3004, 'fixed':  1}}
+DB = {'GM_games'               : {'games':  20, 'moves':  1519, 'fixed':   0},
+      'ambiguous'              : {'games':   3, 'moves':   194, 'fixed':   2},
+      'bali02'                 : {'games':  16, 'moves':  1485, 'fixed':   0},
+      'chessdoctor'            : {'games':  55, 'moves':  4648, 'fixed': 249},
+      'd00_chess_informant'    : {'games': 103, 'moves':  7019, 'fixed':   0},
+      'electronic_campfire'    : {'games':   6, 'moves':   286, 'fixed':   2},
+      'europe_echecs'          : {'games': 307, 'moves': 21024, 'fixed':  15},
+      'exeter_lessons_from_tal': {'games':   8, 'moves':   628, 'fixed':   0},
+      'famous_games'           : {'games': 500, 'moves': 28796, 'fixed':  60},
+      'great_masters'          : {'games':  13, 'moves':   872, 'fixed':  34},
+      'hartwig'                : {'games':  29, 'moves':  2203, 'fixed':   2},
+      'hayes'                  : {'games':   9, 'moves':   637, 'fixed':   0},
+      'human_computer'         : {'games':  29, 'moves':  2290, 'fixed':   0},
+      'immortal_games'         : {'games':  36, 'moves':  2026, 'fixed':   8},
+      'kramnik'                : {'games':  40, 'moves':  3293, 'fixed':   1},
+      'middleg'                : {'games': 481, 'moves': 31560, 'fixed':  19},
+      'moscow64'               : {'games':  42, 'moves':  3760, 'fixed':   0},
+      'newyork1924'            : {'games': 110, 'moves': 10517, 'fixed':  27},
+      'perle'                  : {'games': 194, 'moves': 14922, 'fixed':  11},
+      'polgar'                 : {'games':  40, 'moves':  3348, 'fixed':   0},
+      'pon_korch'              : {'games':   8, 'moves':  1003, 'fixed':   0},
+      'romero'                 : {'games':   4, 'moves':   363, 'fixed':   0},
+      'russian_chess'          : {'games':  10, 'moves':   725, 'fixed':   8},
+      'scarborough_2001'       : {'games': 385, 'moves': 30137, 'fixed':  15},
+      'scca'                   : {'games':  75, 'moves':  5114, 'fixed':   6},
+      'schiller'               : {'games':   8, 'moves':   681, 'fixed':   0},
+      'semicomm'               : {'games':  46, 'moves':  3004, 'fixed':   1}}
 
 
 FIND_TEST = {
@@ -77,6 +77,7 @@ def check_result(output, fname, stats):
         ok3 = DB[fname]['fixed'] == fixed
         return 'OK' if ok1 and ok2 and ok3 else 'FAIL'
 
+
 def run_file(file, stats):
     fname = os.path.basename(file)
     sys.stdout.write('Processing ' + fname + '...')
@@ -85,12 +86,14 @@ def run_file(file, stats):
     result = check_result(output, fname, stats)
     print(result)
 
+
 def run_find_test(fname, expected_output):
     sys.stdout.write('Processing ' + fname + ' for find test')
     output = qx(["./parser", 'find', fname, expected_output['input']], stderr=STDOUT)
     assert ast.literal_eval(output) == expected_output['output']
     sys.stdout.write('...OK')
     sys.stdout.flush()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run test on pgn files')
@@ -106,7 +109,7 @@ if __name__ == "__main__":
     for fn in files:
         run_file(fn, stats)
 
-    for k,v in FIND_TEST.items():
+    for k, v in FIND_TEST.items():
         run_find_test(args.dir+k, v)
 
     print("\ngames {}, moves {}, fixed {}\n"
